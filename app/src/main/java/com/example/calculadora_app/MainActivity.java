7d6ee2d1dc9f2609268233115ffd74f97c52ce4c
FelipeMYung <102309048+FelipeMYung@users.noreply.github.com>
2024-07-05 09:19:14 -0300

    funcionalidade calc

--- app/src/main/java/com/example/calculadora_app/MainActivity.java
+++ app/src/main/java/com/example/calculadora_app/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.calculadora_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,71 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton btnSoma = findViewById(R.id.btnSoma);
+        btnSoma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText numero1 = findViewById(R.id.numero1);
+                int n1 = Integer.parseInt(numero1.getText().toString());
+
+                EditText numero2 = findViewById(R.id.numero2);
+                int n2 = Integer.parseInt(numero2.getText().toString());
+
+                int resultado = (n1+n2);
+
+                TextView txtResultado = findViewById(R.id.txtResultado);
+                txtResultado.setText(Integer.toString(resultado));
+            }
+        });
+        ImageButton btnMenos = findViewById(R.id.btnMenos);
+        btnMenos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText numero1 = findViewById(R.id.numero1);
+                int n1 = Integer.parseInt(numero1.getText().toString());
+
+                EditText numero2 = findViewById(R.id.numero2);
+                int n2 = Integer.parseInt(numero2.getText().toString());
+
+                int resultado = (n1-n2);
+
+                TextView txtResultado = findViewById(R.id.txtResultado);
+                txtResultado.setText(Integer.toString(resultado));
+            }
+        });
+
+        ImageButton btnDivisao = findViewById(R.id.btnDivisao);
+        btnDivisao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText numero1 = findViewById(R.id.numero1);
+                int n1 = Integer.parseInt(numero1.getText().toString());
+
+                EditText numero2 = findViewById(R.id.numero2);
+                int n2 = Integer.parseInt(numero2.getText().toString());
+
+                int resultado = (n1/n2);
+
+                TextView txtResultado = findViewById(R.id.txtResultado);
+                txtResultado.setText(Integer.toString(resultado));
+            }
+        });
+        ImageButton btnMultiplicacao = findViewById(R.id.btnMultiplicacao);
+        btnMultiplicacao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText numero1 = findViewById(R.id.numero1);
+                int n1 = Integer.parseInt(numero1.getText().toString());
+
+                EditText numero2 = findViewById(R.id.numero2);
+                int n2 = Integer.parseInt(numero2.getText().toString());
+
+                int resultado = (n1*n2);
+
+                TextView txtResultado = findViewById(R.id.txtResultado);
+                txtResultado.setText(Integer.toString(resultado));
+            }
+        });
     }
 }
